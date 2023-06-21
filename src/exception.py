8618368a8d73e86226d689f7c3f666ad7e42f6a0
root <root@ip-172-31-22-156.ec2.internal>
2023-06-21 05:22:09 +0000

    Data Tranformations Done

--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,4 @@
-from setuptools import find_packages,setup
-from typing import List
-
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-   import sys
+import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
